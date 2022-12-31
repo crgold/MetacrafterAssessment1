2cea0bb9c91d712174d74ab9b50f482332332bac
Chris <crgold@asu.edu>
2022-12-31 22:05:36 +0800

    comment out console.sol

--- Assessments/Assessment2/contracts/Assessment.sol
+++ Assessments/Assessment2/contracts/Assessment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 
 contract Assessment {
     address payable public owner;
