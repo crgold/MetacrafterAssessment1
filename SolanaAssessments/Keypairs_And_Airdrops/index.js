add06c9690918e5add6ac3053934009721694565
Chris <crgold@asu.edu>
2023-01-06 03:48:12 +0800

    trying to fix transfer function

--- SolanaAssessments/Keypairs_And_Airdrops/index.js
+++ SolanaAssessments/Keypairs_And_Airdrops/index.js
@@ -6,7 +6,8 @@ const {
     Keypair,
     LAMPORTS_PER_SOL,
     Transaction,
-    SystemProgram
+    SystemProgram,
+    sendAndConfirmTransaction
 } = require("@solana/web3.js");
 const { get } = require("http");
 
@@ -17,26 +18,20 @@ const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 const sender = new Keypair();
 const receiver = new Keypair();
 
-// Exact the public and private key from the keypair for sender
+// Exact the public key from the keypair for sender
 const senderPublicKey = new PublicKey(sender._keypair.publicKey).toString();
-const senderPrivateKey = sender._keypair.secretKey;
 
-// Exact the public and private key from the keypair for receiver
+// Exact the public from the keypair for receiver
 const receiverPublicKey = new PublicKey(receiver._keypair.publicKey).toString();
-const receiverPrivateKey = receiver._keypair.secretKey;
 
 console.log("Public Key of generated sender keypair", senderPublicKey);
 console.log("Public Key of generated receiver keypair", receiverPublicKey);
-
-// Make wallets (keypairs) from private keys
-const senderWallet = await Keypair.fromSecretKey(senderPrivateKey);
-const receiverWallet = await Keypair.fromSecretKey(receiverPrivateKey);
 
 
 // Get the wallet balance from a given private key
 const getWalletBalance = async (userPublicKey) => {
     try { 
-        return await connection.getBalance(userPublicKey);
+        return await connection.getBalance(new PublicKey(userPublicKey));
     } catch (err) {
         console.log(err);
     }
@@ -47,7 +42,7 @@ const airDropSol = async () => {
         // Request airdrop of 2 SOL to the sender wallet
         console.log("Airdropping some SOL to sender wallet!");
         const fromAirDropSignature = await connection.requestAirdrop(
-            sender.publicKey,
+            new PublicKey(senderPublicKey),
             2 * LAMPORTS_PER_SOL
         );
         await connection.confirmTransaction(fromAirDropSignature);
@@ -56,15 +51,22 @@ const airDropSol = async () => {
     }
 };
 
-const transferSol = async (sender, receiver, amount) => {
+const transferSol = async (from, to, amount) => {
     try {
         const transaction = new Transaction().add(
             SystemProgram.transfer({
-                fromPubkey: sender,
-                toPubkey: receiver,
-                lamports: amount * LAMPORTS_PER_SOL
+                fromPubkey: from,
+                toPubkey: to,
+                lamports: amount
             })
-        )
+        );
+
+        let signature = await sendAndConfirmTransaction(
+            connection,
+            transaction,
+            [sender]
+        );
+        console.log(`Signature of transfer is ${signature}`);
     } catch (err) {
         console.log(err);
     }
@@ -76,15 +78,16 @@ const mainFunction = async () => {
     await airDropSol();
 
     // Display balances of wallets
-    console.log(`Sender wallet balance: ${await getWalletBalance(senderPublicKey)}`);
-    console.log(`Receiver wallet balance: ${await getWalletBalance(receiverPublicKey)}`)
+    console.log(`Sender wallet balance: ${await getWalletBalance(senderPublicKey) / LAMPORTS_PER_SOL} SOL`);
+    console.log(`Receiver wallet balance: ${await getWalletBalance(receiverPublicKey) / LAMPORTS_PER_SOL} SOL`)
 
     // Transfer 50% of sender balance to receiver
-    transferSol(senderPublicKey, receiverPublicKey, await getWalletBalance(senderPublicKey) * 0.5);
+    console.log("Transferring 50% of sender wallet to receiver wallet");
+    transferSol(senderPublicKey, receiverPublicKey, (await getWalletBalance(senderPublicKey) * 0.5));
 
     // Display new balances
-    console.log(`Sender wallet balance: ${await getWalletBalance(senderPublicKey)}`);
-    console.log(`Receiver wallet balance: ${await getWalletBalance(receiverPublicKey)}`)
+    console.log(`Sender wallet balance: ${await getWalletBalance(senderPublicKey) / LAMPORTS_PER_SOL} SOL`);
+    console.log(`Receiver wallet balance: ${await getWalletBalance(receiverPublicKey) / LAMPORTS_PER_SOL} SOL`)
 }
 
 mainFunction();
